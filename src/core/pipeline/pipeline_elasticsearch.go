102877ff16c036bcb3c2621ee66d7117a2f644f1
Domon <domonji95@gmail.com>
2018-03-30 14:44:23 +0800

    http

--- src/core/pipeline/pipeline_elasticsearch.go
+++ src/core/pipeline/pipeline_elasticsearch.go
@@ -1,12 +1,15 @@
 package pipeline
 
 import (
+	"io/ioutil"
+	"strings"
 		"../common/com_interfaces"
 		"../common/page_items"
+    "net/http"
 )
 
 type PipelineElasticsearch struct {
-    url string
+		url string
 }
 
 func NewPipelineElasticsearch(url string) *PipelineElasticsearch {
@@ -15,5 +18,15 @@ func NewPipelineElasticsearch(url string) *PipelineElasticsearch {
 
 func (this * PipelineElasticsearch) Process(items *page_items.PageItems, t *com_interfaces.Task) {
     println("----------------------------------------------------------------------------------------------")
-    println("Crawled url :\t" + items.GetRequest().GetUrl() + "\n")
+		println("Crawled url :\t" + items.GetRequest().GetUrl() + "\n")
+		resp, err := http.Post(this.url, "application/x-www-form-urlencoded", strings.NewReader("a=b"))
+		if err != nil {
+			println(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			println(err)
+		}
+		println(string(body))
 }
